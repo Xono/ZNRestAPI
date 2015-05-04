e7c0d48c435ff1c60d6c12a63d0e9bf94cfbe4c0
Jonathan Head <jonathan@appscore.com.au>
2015-05-04 17:46:30 +1000

    Initial commit

---
+++ ZNRestAPI/ZNRestAPI.h
@@ -0,0 +1,13 @@
+//
+//  ZNRestAPI.h
+//  ZNRestAPI
+//
+//  Created by Jonathan Head on 4/05/2015.
+//  Copyright (c) 2015 Jonathan Head. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZNRestAPI : NSObject
+
+@end
---
+++ ZNRestAPI/ZNRestAPI.m
@@ -0,0 +1,13 @@
+//
+//  ZNRestAPI.m
+//  ZNRestAPI
+//
+//  Created by Jonathan Head on 4/05/2015.
+//  Copyright (c) 2015 Jonathan Head. All rights reserved.
+//
+
+#import "ZNRestAPI.h"
+
+@implementation ZNRestAPI
+
+@end
